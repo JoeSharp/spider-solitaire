66ae1d7db4349869fe918e939522c378943f8815
Joe Sharp <ratracejoe@hotmail.com>
2023-08-19 14:05:12 +0100

    gh pagrse

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/spider-solitaire/",
   plugins: [react()],
   test: {
     globals: true,
